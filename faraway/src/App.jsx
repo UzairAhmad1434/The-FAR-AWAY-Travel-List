9d44d7c6af93a9561eebbf0a625f584a76ca93da
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-06-15 21:18:07 +0500

    adding components

--- faraway/src/App.jsx
+++ faraway/src/App.jsx
@@ -3,9 +3,25 @@ import "./App.css";
 function App() {
   return (
     <>
-      <h1> 🏝️ FAR AWAY 💼</h1>
+      <Logo />
+      <Form />
     </>
   );
 }
 
+function Logo() {
+  return (
+    <h1 className="p-8 text-center font-monoton text-6xl bg-yellow-600">
+      {" "}
+      🏝️FAR AWAY💼
+    </h1>
+  );
+}
+function Form() {
+  return (
+    <h3 className="p-2 font-quicksand bg-orange-700 text-2xl text-center">
+      What you need for your 😍 trip?
+    </h3>
+  );
+}
 export default App;
