3a597ffcca4ffaa0d8b19806290794464479ecc1
uzairahmad1434 <uzairahmadkhanmalik@gmail.com>
2024-06-16 19:13:13 +0500

    packing list items

--- faraway/src/App.jsx
+++ faraway/src/App.jsx
@@ -6,6 +6,15 @@ function App() {
     { id: 1, description: "Shoes", quantity: 2, packed: false },
     { id: 2, description: "Passports", quantity: 1, packed: true },
     { id: 3, description: "Socks", quantity: 3, packed: false },
+    { id: 4, description: "Shoes", quantity: 2, packed: false },
+    { id: 5, description: "Passports", quantity: 1, packed: true },
+    { id: 6, description: "Socks", quantity: 3, packed: false },
+    { id: 7, description: "Shoes", quantity: 2, packed: false },
+    { id: 8, description: "Passports", quantity: 1, packed: true },
+    { id: 9, description: "Socks", quantity: 3, packed: false },
+    { id: 10, description: "Shoes", quantity: 2, packed: false },
+    { id: 11, description: "Passports", quantity: 1, packed: true },
+    { id: 12, description: "Socks", quantity: 3, packed: false },
   ];
 
   const [items, setItems] = useState(initialItems);
@@ -38,8 +47,8 @@ function Form() {
 
 function PackingList({ items }) {
   return (
-    <div>
-      <ul>
+    <div className="p-12 ">
+      <ul className="text-white  text-xl p-8 border-2 gap-y-10 flex flex-wrap">
         {items.map((item) => (
           <Item key={item.id} item={item} />
         ))}
@@ -49,7 +58,12 @@ function PackingList({ items }) {
 }
 
 function Item({ item }) {
-  return <li>{item.description}</li>;
+  return (
+    <li className="md:w-1/3 w-full text-center">
+      {item.quantity} {item.description}
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Footer({ items }) {
@@ -58,7 +72,7 @@ function Footer({ items }) {
   const packedPercentage = (packedItems / totalItems) * 100;
 
   return (
-    <footer className="absolute bottom-0 bg-blue-300 p-8 text-xl text-center w-full">
+    <footer className="fixed bottom-0 bg-blue-300 p-8 text-xl text-center w-full">
       💼 You have {totalItems} items in your list and you packed {packedItems}{" "}
       items ({packedPercentage.toFixed(2)}%)
     </footer>
